1c87d37595cbd5f2450f360896bbb5f3a5d5e210
RanaSibai <rana.sibai@gmail.com>
2021-09-17 17:31:42 +0400

    Created Docs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  MAPBOX_API_KEY: 'pk.eyJ1IjoicmFuYXNpYmFpIiwiYSI6ImNrdG9jNnRjYjBiY2gyeXBuOXF5anp2anMifQ.BU7jr8KYZhIP57VHaSSykg'
 };
